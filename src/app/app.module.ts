7d170f57e007a96282c80aacce5afde93cffd7b8
Luiz dos Reis <luizdosreis@gmail.com>
2020-08-13 22:21:45 -0300

    create dashboard and core modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { CoreModule } from './modules/core/core.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
