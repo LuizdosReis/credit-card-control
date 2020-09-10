3357116c9da61fa0c3061cd6113fa8ed9a8bdf0a
Luiz dos Reis <luizdosreis@gmail.com>
2020-09-09 22:30:51 -0300

    remove not used parse and adapter

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -2,9 +2,6 @@ import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SortableHeaderDirective } from './directives/sortable-header.directive';
-import { NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
-import { CustomDateParserFormatter } from './adapters/custom-date-parser-formatter';
-import { RepresentedCustomAdapter } from './adapters/represented-custom-adapter';
 
 
 @NgModule({
@@ -18,10 +15,6 @@ import { RepresentedCustomAdapter } from './adapters/represented-custom-adapter'
   exports: [
     HeaderComponent,
     SortableHeaderDirective
-  ],
-  providers: [
-    { provide: NgbDateAdapter, useClass: RepresentedCustomAdapter },
-    { provide: NgbDateParserFormatter, useClass: CustomDateParserFormatter },
   ],
 })
 export class CoreModule { }
